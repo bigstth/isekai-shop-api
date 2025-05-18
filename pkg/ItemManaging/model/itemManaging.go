6e6b8e033bde700f8b87146517db5f7554680241
Bigstth <sitthidsn@gmail.com>
2025-05-19 00:00:26 +0700

    Changed description length

--- pkg/ItemManaging/model/itemManaging.go
+++ pkg/ItemManaging/model/itemManaging.go
@@ -4,7 +4,7 @@ type (
 	ItemCreatingReq struct {
 		AdminID     string
 		Name        string `json:"name" validate:"required,max=60"`
-		Description string `json:"description" validate:"required,max=500"`
+		Description string `json:"description" validate:"required,max=200"`
 		Picture     string `json:"picture" validate:"required,max=500"`
 		Price       uint   `json:"price" validate:"required"`
 	}
@@ -12,7 +12,7 @@ type (
 	ItemEditingReq struct {
 		AdminID     string
 		Name        string `json:"name" validate:"required,max=60"`
-		Description string `json:"description" validate:"required,max=500"`
+		Description string `json:"description" validate:"required,max=200"`
 		Picture     string `json:"picture" validate:"required,max=500"`
 		Price       uint   `json:"price" validate:"required"`
 	}
