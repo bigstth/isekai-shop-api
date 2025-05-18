05a8f2dfeb5e63808941dd339683014222e0bf9f
Bigstth <sitthidsn@gmail.com>
2025-05-18 23:51:52 +0700

    Changed max name length

--- pkg/ItemManaging/model/itemManaging.go
+++ pkg/ItemManaging/model/itemManaging.go
@@ -3,7 +3,7 @@ package model
 type (
 	ItemCreatingReq struct {
 		AdminID     string
-		Name        string `json:"name" validate:"required,max=100"`
+		Name        string `json:"name" validate:"required,max=60"`
 		Description string `json:"description" validate:"required,max=500"`
 		Picture     string `json:"picture" validate:"required,max=500"`
 		Price       uint   `json:"price" validate:"required"`
@@ -11,7 +11,7 @@ type (
 
 	ItemEditingReq struct {
 		AdminID     string
-		Name        string `json:"name" validate:"required,max=100"`
+		Name        string `json:"name" validate:"required,max=60"`
 		Description string `json:"description" validate:"required,max=500"`
 		Picture     string `json:"picture" validate:"required,max=500"`
 		Price       uint   `json:"price" validate:"required"`
